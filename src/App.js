c0b67a518deeae9819a132172000c810611903d7
Ana Carolina <karols.15@hotmail.com>
2021-09-24 20:53:58 -0300

    Importing the userProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { 
+import {
   AppBar,
   Box,
   Paper,
@@ -7,6 +7,7 @@ import {
   Typography
 } from '@material-ui/core';
 
+import { UserProvider } from './providers/user'
 import { ReposController } from './components/repos/repos.controller';
 import { UsersSelectController } from './components/users-select/users-select.controller';
 
@@ -14,22 +15,23 @@ import { UsersSelectController } from './components/users-select/users-select.co
 export default function SearchAppBar() {
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Fidu
-          </Typography>
-          <Paper
-            component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-          >
-          <UsersSelectController></UsersSelectController>
-          </Paper>
-        </Toolbar>
-      </AppBar>
-
-      <ReposController></ReposController>
-    </Box>
+    <UserProvider>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Fidu
+            </Typography>
+            <Paper
+              component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+              <UsersSelectController />
+            </Paper>
+          </Toolbar>
+        </AppBar>
+        <ReposController />
+      </Box>
+    </UserProvider>
   );
 }
