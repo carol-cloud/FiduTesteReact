c19b2ad97d80bfea478abac954dd08a5d613c48a
Ana Carolina <karols.15@hotmail.com>
2021-09-24 15:12:35 -0300

    Creating the handleClick function

--- src/components/user-select/user-select.controller.jsx
+++ src/components/user-select/user-select.controller.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { UserService } from '../../services/users.services';
 
 import { UserSelectView } from './user-select.view';
 
 export const UserSelectController = (props) => {
   const {user} = props;
-  return <UserSelectView user={user} />
+  const userService = UserService();
+
+  const handleClick = async () => {
+    const response = await userService.listRepos(user.login);
+    console.log (await response.json());
+  }
+  return <UserSelectView user={user} handleClick={handleClick} />
 }
 
 
