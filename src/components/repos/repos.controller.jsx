89017123b376a105bcb82db6f63253134dff4118
Ana Carolina <karols.15@hotmail.com>
2021-09-24 20:52:22 -0300

    Creating the logic to set the selected user's repositories

--- src/components/repos/repos.controller.jsx
+++ src/components/repos/repos.controller.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import {UserContext} from '../../providers/user';
+import { UserService } from '../../services/users.services';
 
 import { ReposView } from './repos.view';
 
 export const ReposController = () => {
+  const { userSelected } = useContext(UserContext);
+  const userService = UserService();
+  const [ reposUserSelected, setReposUserSelected ] = useState([]);
 
+  const getRepos = async () => {
+    const response = await userService.listRepos(userSelected.login);
+    const repos = await response.json();
+    setReposUserSelected(repos);
+  }
+
+  useEffect(() => {
+    if (userSelected.login) {
+      getRepos();
+    }
+  }, [userSelected])
+  
   return <ReposView />
 }
 
